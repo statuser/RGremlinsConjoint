613e88cd8e6fcec9d05f7360902a81f6303b4e55
John R. Howell <john_howell@byu.edu>
2021-04-15 16:34:17 -0600

    Working on Atchade parameters

--- R/gremlinsUtilityFunctions.R
+++ R/gremlinsUtilityFunctions.R
@@ -11,11 +11,11 @@ calculate_segment_memberships <- function(betai, bayesm_data) {
   num_respondents <- dim(betai)[1]
   num_iterations <- dim(betai)[3]
   num_concepts <- bayesm_data$p
-  num_tasks <- nrow(bayesm_data$lgtdata[[resp]]$X)/num_concepts
+  num_tasks <- nrow(bayesm_data$lgtdata[[1]]$X)/num_concepts
 
   hit_rates <- double(num_respondents)
   # Convert to vectorized version to avoid doubel loop and speed things up
-  for( resp in 1:num_respondents) {
+  for(resp in 1:num_respondents) {
     x_beta <- array((bayesm_data$lgtdata[[resp]]$X %*% betai[resp,,]), c(num_concepts, num_tasks, num_iterations))
     choices <- apply(x_beta, c(2, 3), which.max)
     hit_rates[resp] <- mean(choices == matrix(bayesm_data$lgtdata[[resp]]$y, nrow=16, ncol=num_iterations))
@@ -45,4 +45,33 @@ calculate_segment_memberships <- function(betai, bayesm_data) {
 
 
   return(segments)
+}
+
+set_atchade_slope_parameters <- function(slopeBar, atchade_tuning_factor, nParameters, nRespondents) {
+  slope_parameters <- rep(
+    list(
+      list(
+        metstd = rep(atchade_tuning_factor, times=nParameters),
+        Vmet = rep(1, times=nParameters),
+        mu_adapt = slopeBar,
+        Gamma_adapt = rep(1, times=nParameters)
+      )
+    ),
+  times= nRespondents)
+
+  return(slope_parameters)
+}
+
+set_atchade_lambda_parameters <- function(lambda, nClusters, estimated_mean_lambda, estimated_var_lambda) {
+  estimated_mean_lambda[1] <- 1
+  estimated_var_lambda[1] <- 1
+
+  lambda_parameters <- list(
+    metstd = c(NA, rep(lambda, times = (nClusters - 1))),
+    Vmet = c(NA, estimated_var_lambda[2:nClusters]),
+    mu_adapt = estimated_mean_lambda,
+    Gamma_adapt = estimated_var_lambda
+  )
+
+  return(lambda_parameters)
 }
--- data-raw/Example Analysis.R
+++ data-raw/Example Analysis.R
@@ -29,18 +29,22 @@ bayesm_data <- convert_to_bayesm(cameraData, codedCamera)
 bayesm_prior <- list(ncomp=1)
 starting_value_iters <- 2000
 starting_value_thin <- 10
-starting_values_burnin <- 1750
+starting_value_burnin <- 1750
 bayesm_mcmc <- list(R=starting_value_iters*starting_value_thin, keep=starting_value_thin, nprint = 0)
 
 invisible(capture.output(bayesm_results <- rhierMnlRwMixture(Data=bayesm_data, Prior=bayesm_prior, Mcmc=bayesm_mcmc)))
 
-
-slope_start <- apply(bayesm_results$betadraw[,,starting_values_burnin:starting_value_iters], c(1,2), mean)
-slopeBar_start <- colMeans(betai_start)
+# Set Starting Values
+slope_start <- apply(bayesm_results$betadraw[,,starting_value_burnin:starting_value_iters], c(1,2), mean)
+slopeBar_start <- colMeans(slope_start)
 slopeCov_start <- diag(length(slopeBar_start))
 lambda_start <- seq(1, 50, length.out = num_lambda_segments)
 segment_membership_start <- calculate_segment_memberships(bayesm_results$betadraw[,,starting_values_burnin:starting_value_iters], bayesm_data)
 phi_lambda <- as.numeric(table(segment_membership_start)/length(segment_membership_start))
+
+#Starting Values for Atchade algorithm
+Atch_starting_values_slopes <- set_atchade_slope_parameters(slopeBar_start, 0.1, length(slopeBar_start), nrow(slope_start))
+Atch_starting_values_lambda <- set_atchade_lambda_parameters(15, 2, c(NA, 20), c(NA, 15))
 
 
 
