6db272e28181f1dd994a2ee654ebbce35c141711
John R. Howell <john_howell@byu.edu>
2021-05-20 14:49:06 -0600

    Note for future work

--- R/RGremlinsConjoint.R
+++ R/RGremlinsConjoint.R
@@ -25,6 +25,8 @@ NULL
 #' Set global options for the gremlins models.  These options are not expected to be modified by the user
 #' but are extracted from the functions to simplify the coding.
 gremlinsEnv <- new.env()
+
+# TODO create functions to set these values
 gremlinsEnv$jumpSizes <- c(0.05, 0.1, 0.5)
 gremlinsEnv$jumpSizeProbs <- c(0.65, 0.25, 0.10)
 gremlinsEnv$totalConstraintTries <- 100
